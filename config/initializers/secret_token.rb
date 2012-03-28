d72db1463678789d9e1ffdcca9fba7c0d2543d38
oliverbarreto <oliver.barreto.online@gmail.com>
2012-03-28 22:11:29 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tkiero::Application.config.secret_token = '6dc8e13495532b89703a45720402dd85a2fad4e6bad170681731846789621fb38092fa74e0e0a4d215277c4ba31133c9b98a544859f596292cfdab0cc16b7c8e'
