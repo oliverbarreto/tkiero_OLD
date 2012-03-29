cb6938b18eeec494d1a79ec9633a41b3f80caca4
Oliver <oliver.barreto.online@gmail.com>
2012-03-29 06:37:27 +0100

    Added Static Pages Home & Help: MVC & Routes.rb

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+
+  #Static Page of the site - HOME
+  def home
+  end
+
+  #Static Page of the site - HELP
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Tkiero::Application.routes.draw do
+  
+  #Static Pages of the App
+  get "static_pages/home"
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
