dfde076d0b0e9720bea5398c2d010eb1a754fea9
Oliver <oliver.barreto.online@gmail.com>
2012-03-31 19:47:29 +0100

    Added static page contact and created skeleton for others

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,23 +1,24 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
+  let(:base_title) { "tKiero Demo App" }
+
+  
   describe "Home page" do
     #it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
     #  get static_pages_index_path
     #  response.status.should be(200)
     #end
-    
-    # Test for Home Page Content
-#    it "should have the content 'Build One IDeas'" do
-#     visit '/static_pages/home'
-#      page.should have_content('Build One IDeas')
-#    end
-    
-    it "should have the right title" do
+        
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-                    :text => "tKiero Demo App | Home")
+      page.should have_selector('title', 
+                    :text => "#{base_title}")
+
+#      page.should have_selector('title',
+#                    :text => "tKiero Demo App")
     end
  
     it "should have the h1 'Sample App'" do
@@ -25,6 +26,13 @@ describe "StaticPages" do
       page.should have_selector('h1', 
                     :text => 'tKiero Demo App')
     end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', 
+                    :text => '| Home')
+    end
+    
   end
   
   describe "Help page" do
@@ -36,16 +44,22 @@ describe "StaticPages" do
 #    end
 
     # Test for Help Page Tittle Content
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                    :text => "tKiero Demo App | Help")
+                    :text => "tKiero Demo App")
     end
 
     it "should have the h1 'Sample App'" do
       visit '/static_pages/help'
       page.should have_selector('h1', 
                     :text => 'tKiero Demo App')
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/help'
+      page.should_not have_selector('title', 
+                    :text => '| Help')
     end
   end  
    
@@ -58,10 +72,10 @@ describe "StaticPages" do
 #    end
     
     # Test for About Page Tittle Content
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "tKiero Demo App | About Us")
+                    :text => "tKiero Demo App")
     end
  
     it "should have the h1 'Sample App'" do
@@ -71,4 +85,22 @@ describe "StaticPages" do
     end
   end  
 
+    it "should not have a custom page title" do
+      visit '/static_pages/about'
+      page.should_not have_selector('title', 
+                    :text => '| About')
+    end
+
+  describe "Contact page" do
+    it "should exist and have a basic content 'Build One IDeas'" do
+      visit '/static_pages/contact'
+      page.should have_content('tKiero Demo App')
+    end
+
+    it "should have the right base title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "tKiero Demo App")
+    end
+  end
 end
